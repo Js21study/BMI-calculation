4fde50a95c0a8c410294316bad9f11800bf5c812
Js21study <93077840+Js21study@users.noreply.github.com>
2023-02-02 15:14:08 +0200

    Update CountModel.js

--- src/components/CountModel.js
+++ src/components/CountModel.js
@@ -4,12 +4,12 @@ function CountModel({name, value, setValue}) {
    
   return (
     <div style={{margin: "50px"}}>
-         <input  type="number" value={value} onChange={(e) => setValue(e.target.value)} />
+         <input  type="number" value={Number(value)} onChange={(e) => setValue(Number(e.target.value))} />
         <h4>{name}</h4>
         <h1>{value}</h1>
         <div className="btn-group">
-        <button className="minus" onClick={() => setValue(value-1)}>-</button>
-        <button className="plus" onClick={() => setValue(value+1)}>+</button>
+        <button className="minus" onClick={() => setValue(Number(value)-1)}>-</button>
+        <button className="plus" onClick={() => setValue(Number(value)+1)}>+</button>
         </div>
        
     </div>
